6f516fbdee482935b603e0444ef846f4a8fafea0
zhuang biaowei <zbw@kaiyuanshe.org>
2025-04-09 21:17:20 +0800

    new post 关于博客的博客

---
+++ Gemfile
@@ -0,0 +1,5 @@
+ruby "3.3.4"
+
+gem "smart_prompt"
+gem "smart_agent"
+gem "numo-narray", "~> 0.9.2.1"
--- agents/writer.rb
+++ agents/writer.rb
@@ -1,8 +1,10 @@
 SmartAgent.define :writer do
   result = call_worker(:blog_metadata, params, with_tools: true, with_history: true)
   if result.call_tools
-    filename = call_tools(result)
+    filename = call_tools(result).first
     content = call_worker(:generate_blog, params, with_tools: false, with_history: true)
+    content = content.response
+    content = content.match(/```html\n(.*?)```/m)[1]
     f = File.new("./docs/posts/" + filename, "w")
     f.puts content
     f.close
